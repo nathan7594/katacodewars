51e1250f26ce09fcb29f982cf166a33b374a41d0
Nathan7594 <nathan.75@outlook.com>
2023-06-19 11:51:57 +0200

    Final Push Exercice reverse 19/06/2023

--- reverse.js
+++ reverse.js
@@ -18,14 +18,10 @@ function spinWords(string) {
 }
 
 spinWords("Hey fellow warriors");
-//   let array = []
-//   for (i=0; i < words.length; i++) {
-//     if (words[i].length >= 5) {
-//       let reverse = words[i].split("").reverse().join("");
-//       array.push(reverse)
-//     } 
-//     else{
-//         array.push(words)
-//         console.log(array)
-//     }
-//    }
+// Enoncé : Écrivez une fonction qui prend une chaîne d'un ou plusieurs mots et renvoie la même chaîne, 
+// mais avec les mots de cinq lettres ou plus inversés (tout comme le nom de ce Kata). 
+// Les chaînes transmises seront composées uniquement de lettres et d'espaces. 
+// Les espaces seront inclus uniquement lorsque plus d'un mot est présent.
+// spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" 
+// spinWords( "This is a test") => returns "This is a test" 
+// spinWords( "This is another test" )=> returns "This is rehtona test"
